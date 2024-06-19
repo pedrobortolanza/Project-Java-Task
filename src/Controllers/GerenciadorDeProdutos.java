13e4115fc6d0150eb95a7739d82a4cd619ca1be8
Gabriel Ribeiro Batista <gabriel.r.batista@outlook.com>
2024-06-19 14:19:50 -0300

    Criação e implementação dos models e estruturação do projeto

---
+++ src/Controllers/GerenciadorDeProdutos.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+public class GerenciadorDeProdutos 
+{
+    //Favor criar os métodos
+    //registrarProduto
+    //alterarProduto
+    //cadastrarProduto
+    //Criar o arquivo Produtos.txt
+    //Criar o método consultarProduto
+}
